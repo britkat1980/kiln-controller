6b92d9987a6743e5904c79f6a5724f7656291f06
Mark Cracknell <mark@marksweb.org.uk>
2026-03-24 23:26:19 +0000

    n

--- lib/ovenWatcher.py
+++ lib/ovenWatcher.py
@@ -107,7 +107,7 @@ class OvenWatcher(threading.Thread):
                             if topic in["runtime", "totaltime"]:
                                 td = timedelta(seconds=oven_state[topic])
                                 result = self.client.publish(config.mqtt_kiln_name+"/"+topic, str(td))
-                            elif isinstance(oven_state[topic],[int, float]):
+                            elif isinstance(oven_state[topic],(int, float)):
                                 result = self.client.publish(config.mqtt_kiln_name+"/"+topic, round(oven_state[topic],2))
                             else:
                                 result = self.client.publish(config.mqtt_kiln_name+"/"+topic, oven_state[topic])
