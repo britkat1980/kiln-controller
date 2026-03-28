cca0a820e2c3d664f6168f44517cc871106edd2b
Mark Cracknell <mark@marksweb.org.uk>
2026-03-28 10:41:46 +0000

    fix

--- lib/ovenWatcher.py
+++ lib/ovenWatcher.py
@@ -112,7 +112,7 @@ class OvenWatcher(threading.Thread):
                             elif isinstance(oven_state[topic],(int, float)):
                                 result = self.client.publish(config.mqtt_kiln_name+"/"+topic, round(oven_state[topic],2))
                             elif topic=="profile":
-                                if not oven_state.state == "RUNNING":
+                                if not oven_state["state"] == "RUNNING":
                                     result = self.client.publish(config.mqtt_kiln_name+"/"+topic, "IDLE")
                                 else:
                                     result = self.client.publish(config.mqtt_kiln_name+"/"+topic, oven_state[topic])
